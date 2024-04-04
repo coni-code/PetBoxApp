189c9546360b7a759d903618f0e944129e20d367
bartek3xe <bartek3xe@gmail.com>
2024-04-04 19:30:41 +0200

    Setup docker

--- client/src/components/First.js
+++ client/src/components/First.js
@@ -4,14 +4,10 @@ function First() {
     const [backendData, setBackendData] = useState([{}]);
 
     useEffect(() => {
-        fetch("/api")
+        fetch('http://localhost:5000/api')
             .then(response => response.json())
-            .then(data => {
-                setBackendData(data);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+            .then(data => setBackendData(data))
+            .catch(error => console.error('Error fetching data:', error));
     }, []);
 
     return (
