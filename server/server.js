b7c388fcd4dc855f057e248882ace8db0ffab779
KiviGibson <poplawski143@gmail.com>
2024-04-11 16:09:16 +0000

    add auth routes and json middlewere to handle post request

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const app = express();
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require('./routes/userRoutes');
+
+mongoose.connect('mongodb://db:27017/db')
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
-
-const userRoutes = require('./routes/userRoutes');
+app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] });
 });
 
-app.listen(5000, () => {
+app.listen(5001, () => {
     console.log("Server started on port 5000");
 });
