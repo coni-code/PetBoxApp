3877d9be2df20e0856c669ffe641bdd0174aba92
KiviGibson <poplawski143@gmail.com>
2024-04-11 16:34:03 +0000

    add read and set cookies route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
@@ -13,11 +14,22 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] });
+});
+
+app.get('/set-cookies', (req,res) =>{
+    res.cookie('newUser', false, {httpOnly: true});
+    res.send("hi!");
+});
+
+app.get('/read-cookies', (req,res) =>{
+    const cookies = req.cookies;
+    res.send(cookies.newUser);
 });
 
 app.listen(5001, () => {
