a0df7c0016dbd936309ea1b4451714b272873ff4
KiviGibson <poplawski143@gmail.com>
2024-04-15 14:01:45 +0000

    add static function to search for user in database by login and password

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     login: {
         type: String,
         required: [true, 'Login is required'],
+        unique: [true, `name in use`],
     },
     email: {
         type: String, 
@@ -24,7 +25,18 @@ userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
-})
+});
+
+userSchema.statics.login = async function(login, password){
+    const user = await this.findOne({login});
+    if(user){
+        auth = await bcrypt.compare(password, user.password);
+        if(auth){
+            return user;
+        }
+    }
+    throw Error('incorrect credentials!');
+}
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
