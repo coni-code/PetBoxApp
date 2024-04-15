fd75071e54ad9c507e8ceaa798350b4f1ae3ae6e
Michal <hardcorpl5@gmail.com>
2024-04-15 17:51:32 +0200

    Improve validation for sing up

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -4,24 +4,11 @@ export const Login = (props) => {
 
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
-    const [emptyFields, setEmptyFields] = useState([]);
+
+    console.log(login);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        const emptyFieldsArray = [];
-        if (login.trim() === '') {
-            emptyFieldsArray.push('login');
-        }
-        if (password.trim() === '') {
-            emptyFieldsArray.push('password');
-        }
-        setEmptyFields(emptyFieldsArray);
-
-        if (emptyFieldsArray.length > 0) {
-            alert("Nie wszystkie pola są uzupełnione.");
-            return;
-        }
     }
 
     return ( 
@@ -33,33 +20,25 @@ export const Login = (props) => {
                     <input 
                         value={login} 
                         onChange={(e) => {
-                            setLogin(e.target.value);
-                            if (e.target.value.trim() !== '') {
-                                setEmptyFields(emptyFields.filter(field => field !== 'login'));
-                            }
+                            setLogin(e.target.value);                     
                         }} 
                         type="text" 
                         placeholder="Login" 
                         id="login" 
                         name="login" 
-                        className={emptyFields.includes('login') ? 'empty-field' : ''}
                     />
                     <label htmlFor="Password">Password</label>
                     <input 
                         value={password} 
                         onChange={(e) => {
                             setPassword(e.target.value);
-                            if (e.target.value.trim() !== '') {
-                                setEmptyFields(emptyFields.filter(field => field !== 'password'));
-                            }
                         }}  
                         type="password" 
                         placeholder="Password" 
                         id="password" 
                         name="password"
-                        className={emptyFields.includes('password') ? 'empty-field' : ''}
                     />
-                    <button class="log-button" type="submit">Log In</button>
+                    <button className="log-button" type="submit">Log In</button>
                 </form>
                 <hr/>
                 <button className='switch-button' onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
--- client/src/components/Auth/SingUp.jsx
+++ client/src/components/Auth/SingUp.jsx
@@ -6,48 +6,30 @@ export const SingUp = (props) => {
     const [email, setEmail] = useState('');
     const [password1, setPassword1] = useState('');
     const [password2, setPassword2] = useState('');
-    const [emptyFields, setEmptyFields] = useState([]);
+    const [loginFocused, setLoginFocused] = useState(false);
+    const [emailFocused, setEmailFocused] = useState(false);
+    const [password1Focused, setPassword1Focused] = useState(false);
+    const [password2Focused, setPassword2Focused] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        const emptyFieldsArray = [];
-        if (login.trim() === '') {
-            emptyFieldsArray.push('login');
-        }
-        if (email.trim() === '') {
-            emptyFieldsArray.push('email');
-        }
-        if (password1.trim() === '') {
-            emptyFieldsArray.push('password1');
-        }
-        if (password2.trim() === '') {
-            emptyFieldsArray.push('password2');
-        }
-        setEmptyFields(emptyFieldsArray);
-
-        if (emptyFieldsArray.length > 0) {
-            alert("Nie wszystkie pola są uzupełnione.");
-            return;
-        }
-
-        const loginRegex = /^[a-zA-Z0-9]+$/;
-        if (!loginRegex.test(login)) {
-            alert("Login może zawierać tylko litery i cyfry");
-            return;
-        }
-
-        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
-         if (!passwordRegex.test(password1)) {
-             alert("Hasło musi zaczynać się dużą literą i mieć jeden znak specjalny");
-            return;
-        }
-
-        if (password1 !== password2) {
-            alert("Hasła się różnią");
-            return;
-        }
     }
+
+    const handleLoginFocus = () => {
+        setLoginFocused(true);
+    };
+    
+    const handleEmailFocus = () => {
+        setEmailFocused(true);
+    };
+    
+    const handlePassword1Focus = () => {
+        setPassword1Focused(true);
+    };
+    
+    const handlePassword2Focus = () => {
+        setPassword2Focused(true);
+    };
 
     return ( 
         <>
@@ -59,62 +41,68 @@ export const SingUp = (props) => {
                         value={login} 
                         onChange={(e) => {
                             setLogin(e.target.value);
-                            if (e.target.value.trim() !== '') {
-                                setEmptyFields(emptyFields.filter(field => field !== 'login'));
-                            }
                         }} 
+                        onBlur={handleLoginFocus}
                         type="text" 
                         placeholder="Login" 
                         id="login" 
                         name="login"
-                        className={emptyFields.includes('login') ? 'empty-field' : ''}
+                        focused={loginFocused.toString()}
+                        pattern="^[A-Za-z0-9]{3,16}$"
+                        required
                     />
+                    <span className="error1">Login powinien mieć od 3 do 16 znaków i zawierać tylko litery i cyfry</span>
                     <label htmlFor='Email'>Email</label>
                     <input 
                         value={email} 
                         onChange={(e) => {
                             setEmail(e.target.value);
-                            if (e.target.value.trim() !== '') {
-                                setEmptyFields(emptyFields.filter(field => field !== 'email'));
-                            }
-                        }}  
+                        }}
+                        onBlur={handleEmailFocus}  
                         type='email' 
                         placeholder='Email' 
                         name='email' 
                         id='email'
-                        className={emptyFields.includes('email') ? 'empty-field' : ''}
+                        focused={emailFocused.toString()}
+                        required
                     />
+                    <span className="error2">E-mail powinien zawierać małpę</span>
                     <label htmlFor="Password">Password</label>
                     <input 
                         value={password1} 
                         onChange={(e) => {
                             setPassword1(e.target.value);
-                            if (e.target.value.trim() !== '') {
-                                setEmptyFields(emptyFields.filter(field => field !== 'password1'));
-                            }
                         }} 
+                        onBlur={handlePassword1Focus}
                         type="password" 
                         placeholder="Password" 
-                        id="password" 
-                        name="password"
-                        className={emptyFields.includes('password1') ? 'empty-field' : ''}
+                        id="password1" 
+                        name="password1"
+                        focused={password1Focused.toString()}
+                        pattern="^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$"
+                        required
                     />
+                    <span className="error3">Hasło musi mieć 8-20 znaków i zawierać litery, 1 cyfrę, 1 znak specjalny</span>
                     <label htmlFor="RepeatPassword">Repeat Password</label>
                     <input 
                         value={password2} 
                         onChange={(e) => {
                             setPassword2(e.target.value);
-                            if (e.target.value.trim() !== '') {
-                                setEmptyFields(emptyFields.filter(field => field !== 'password2'));
-                            }
                         }}  
+                        onBlur={handlePassword2Focus}
+                        onFocus={() => {
+                            password2.name === "password2" && setPassword2Focused(true);
+                        }}
                         type="password" 
                         placeholder="Password" 
-                        id="password" 
-                        name="password"
-                        className={emptyFields.includes('password2') ? 'empty-field' : ''}
+                        id="password2" 
+                        name="password2"
+                        focused={password2Focused.toString()}
+                        pattern = {password1}
+                        required
                     />
-                    <button class="log-button" type="submit">Sing Up</button>
+                    <span className="error4">Hasła róznią się</span>
+                    <button className="log-button" type="submit">Sing Up</button>
                 </form>
                 <hr/>
                 <button className='switch-button' onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
