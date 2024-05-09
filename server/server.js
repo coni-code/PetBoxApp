4185fe9d7831bcdc85288f55505f5a9d405e3f92
Michal <hardcorpl5@gmail.com>
2024-05-09 14:30:32 +0200

    Delete cors

--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ mongoose.connect('mongodb://localhost:27017/db')
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.use(cors({
-    origin: 'http://localhost:3000'
 }));
 
 app.use(express.json());
