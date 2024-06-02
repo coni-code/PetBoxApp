dac4e800169b54fccafde0246870658a5a8de896
KiviGibson <poplawski143@gmail.com>
2024-06-02 17:35:43 +0000

    add routes to server file

--- server/server.js
+++ server/server.js
@@ -7,23 +7,27 @@ const app = express();
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require('./routes/userRoutes');
 const animalRoutes = require('./routes/animalRoutes');
+const eventRoutes = require('./routes/eventRoutes');
 const {requireAuth} = require('./middlewere/authMiddlewere');
+
 mongoose.connect('mongodb://localhost:27017/db')
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.use(cors({
+    origin: 'http://localhost:3000', 
+    credentials: true,
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/animals',requireAuth ,animalRoutes);
+app.use('/api/event', requireAuth, eventRoutes);
+app.use('/api/animals', requireAuth, animalRoutes);
 
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] });
 });
-
 app.listen(5000, () => {
     console.log("Server started on port 5000");
 });
